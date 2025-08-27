c567e3eb8d3e0c30d6024d0a923237904be4307d
RickGrayExt <rick.krcarpets@gmail.com>
2025-08-27 22:07:07 +0100

    Update Program.cs

--- src/Gateway.Service/Program.cs
+++ src/Gateway.Service/Program.cs
@@ -1,5 +1,15 @@
+using Yarp.ReverseProxy; // make sure YARP is imported
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// Add reverse proxy service
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
+
+// Enable proxy routing
 app.MapReverseProxy();
+
 app.Run();
